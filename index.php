a87e932e7ad6ad9ef548fc750cfca44fcf6f56f8
lmizinski <ukaszm@gmail.com>
2020-04-09 16:14:11 +0200

    Update index.php
    test

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
-<?php
-echo "Hello world ".date("Y-m-d h:i:s", time());
+Hello world <br><br>
+
+Czas serwera: <?= date("Y-m-d h:i:s", time()); ?><br>
+Czas ostatniej zmiany pliku: <?=date("Y-m-d h:i:s",  filemtime(__FILE__));; ?>
